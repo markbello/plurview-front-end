f97494ffaf555fef0d012fd474ba2fa05d61dacc
markbello <mcbello51286@gmail.com>
2018-04-03 17:20:15 -0400

    Resize the logo

--- src/components/app/logo.js
+++ src/components/app/logo.js
@@ -3,7 +3,7 @@ import {Image} from 'semantic-ui-react'
 import React from 'react'
 
 const Logo = (props) => {
-  return (<Image src={logoImage} fluid />)
+  return (<Image src={logoImage} size='medium' centered verticalAlign='middle' />)
 }
 
 export default Logo
