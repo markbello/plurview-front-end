bc3b4aba9d8027c7e5c321c46c5aa62f2c3facf0
markbello <mcbello51286@gmail.com>
2018-04-03 17:24:21 -0400

    Try again to center logo on mobile

--- src/components/app/logo.js
+++ src/components/app/logo.js
@@ -1,9 +1,9 @@
 import logoImage from '../../assets/ticket-logo.svg'
-import {Image} from 'semantic-ui-react'
+import {Image, Segment} from 'semantic-ui-react'
 import React from 'react'
 
 const Logo = (props) => {
-  return (<Image src={logoImage} size='medium' centered verticalAlign='middle' />)
+  return (<Segment basic padded><Image src={logoImage} size='medium' centered verticalAlign='middle' centered /></Segment>)
 }
 
 export default Logo
