ad1818be86316bbfaca9da8744866c2e917ded93
markbello <mcbello51286@gmail.com>
2018-04-04 16:32:58 -0400

    Change Loader spacing on Rave List

--- src/components/app/logo.js
+++ src/components/app/logo.js
@@ -1,9 +1,9 @@
-import logoImage from '../../assets/logo-6.svg'
+import logoImage from '../../assets/ticket-logo.svg'
 import {Image, Segment} from 'semantic-ui-react'
 import React from 'react'
 
 const Logo = (props) => {
-  return (<Segment basic padded><Image src={logoImage} size='medium' centered verticalAlign='middle' centered /></Segment>)
+  return (<Segment basic padded><Image src={logoImage} size='large' centered verticalAlign='middle' centered /></Segment>)
 }
 
 export default Logo
