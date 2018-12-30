23cbfca8a4296bd172f338d8d54b376e80a7f810
markbello <mcbello51286@gmail.com>
2018-12-30 14:07:45 -0500

    bugfix(Redux Devtools): toggle between dev and prod environments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { CookiesProvider } from 'react-cookie';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-// import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 import './semantic/dist/semantic.min.css';
 import App from './App';
@@ -11,17 +10,22 @@ import rootReducer from './reducers/rootReducer';
 import { createStore, applyMiddleware, compose } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-// const store = createStore(rootReducer, applyMiddleware(reduxThunk));
-//The lines below implement redux dev tools
-const store = createStore(rootReducer, compose(applyMiddleware(reduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) )
+
+const storeWithReduxDevtools = createStore(rootReducer, compose(applyMiddleware(reduxThunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()) )
+const storeWithoutReduxDevtools = createStore(rootReducer, applyMiddleware(reduxThunk));
+
+const STORE = process.env.REACT_APP_STAGE === 'dev'
+? storeWithReduxDevtools
+: storeWithoutReduxDevtools;
 
 ReactDOM.render(
-   <Provider store={store}>
+   <Provider store={STORE}>
      <BrowserRouter>
        <CookiesProvider>
-         <App store={store}/>
+         <App />
        </CookiesProvider>
      </BrowserRouter>
  </Provider>, document.getElementById('root')
 );
+
 registerServiceWorker();
