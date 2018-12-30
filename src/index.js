9cf4e0a8cf174b848079bc362b983b5ef3ccd2c5
markbello <mcbello51286@gmail.com>
2018-12-30 14:36:13 -0500

    bugfix(Redux Devtools): try again

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import { CookiesProvider } from 'react-cookie';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import reduxThunk from 'redux-thunk';
-import { composeWithDevTools } from 'redux-devtools-extension';
 import './semantic/dist/semantic.min.css';
 import App from './App';
 import rootReducer from './reducers/rootReducer';
 import { createStore, applyMiddleware, compose } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
 
-
-const store = createStore(rootReducer, composeWithDevTools(), applyMiddleware(reduxThunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const enhancer = composeEnhancers(
+  applyMiddleware(reduxThunk),
+);
+const store = createStore(rootReducer, enhancer);
 
 ReactDOM.render(
    <Provider store={store}>
